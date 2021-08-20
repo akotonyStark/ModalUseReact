84248a0b0dd8eae1205e5016f3e6d166843324ba
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-08-20 09:55:15 +0000

    Update ErrorModal.js

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React from 'react'
 
-import Card from './Card';
-import Button from './Button';
-import classes from './ErrorModal.module.css';
+import Card from './Card'
+import Button from './Button'
+import classes from './ErrorModal.module.css'
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />
+}
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  )
+}
 
 const ErrorModal = (props) => {
-  return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
-  );
-};
+  return <React.Fragment></React.Fragment>
+}
 
-export default ErrorModal;
+export default ErrorModal
